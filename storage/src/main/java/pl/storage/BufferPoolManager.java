6491ff7175b4ba64b10ba2265ab92f0b5fb1e466
Ururu1000 <paharaleksej@gmail.com>
2026-05-03 11:35:36 +0200

    LRUReplacer

---
+++ storage/src/main/java/pl/storage/BufferPoolManager.java
@@ -0,0 +1,9 @@
+package pl.storage;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BufferPoolManager {
+
+}
--- storage/src/main/java/pl/storage/LRUReplacer.java
+++ storage/src/main/java/pl/storage/LRUReplacer.java
@@ -1,4 +1,27 @@
 package pl.storage;
-
+import java.util.LinkedHashSet;
 public class LRUReplacer {
+    private final LinkedHashSet<Integer> unpinnedFrames;
+    private final int capacity;
+
+    public LRUReplacer(int numPages){
+        this.capacity = numPages;
+        this.unpinnedFrames = new LinkedHashSet<>(numPages);
+    }
+    public synchronized void pin(int frameId){
+        unpinnedFrames.add(frameId);
+    }
+    public synchronized void unpin(int frameId){
+        if(unpinnedFrames.size () < capacity){
+            unpinnedFrames.add(frameId);
+        }
+    }
+    public synchronized Integer victim() {
+        if (unpinnedFrames.isEmpty()) {
+            return null;
+        }
+        int victimId = unpinnedFrames.iterator().next();
+        unpinnedFrames.remove(victimId);
+        return victimId;
+    }
 }
